09d70fdaf2337978260b385ff95fdb37917db3bb
Jonas Schiøtt <51338474+JonasSchiott@users.noreply.github.com>
2024-03-12 10:12:13 +0100

    WORKING ON CONNECTING TO LECTIO STILL

--- lib/lectioHandler/Handler.tsx
+++ lib/lectioHandler/Handler.tsx
@@ -1,9 +1,24 @@
-function ConnectToLectio(userLogin: UserLogin): boolean {
+export async function ConnectToLectio({ Username, Password, GymnasiumNumber }: UserLogin): Promise<boolean> {
+    const lectioLoginPage = `https://www.lectio.dk/lectio/${GymnasiumNumber}/login.aspx`;
+
+    const postBody: FormData = new FormData();
+
+    postBody.append('username', Username);
+    postBody.append('password', Password);
+
+    let response: object = {};
+    
+    await fetch(lectioLoginPage, {
+        method: 'POST',
+        body: postBody,
+    }).then(res => response = res.json);
+
+    console.log(response);
 
     return true;
 }
 
-interface UserLogin {
+type UserLogin = {
     Username: string,
     Password: string,
     GymnasiumNumber: number
